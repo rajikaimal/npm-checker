943e093d2dddc4393d27473139a4299ce05c5e74
rajikaimal <inbox.rajika@gmail.com>
2016-09-10 21:04:31 +0530

    npm-checker api

---
+++ index.js
@@ -0,0 +1,24 @@
+const httpFetcher = require('http-fetcher');
+
+module.exports = function(packageName) {
+
+	let found = null;
+
+	return new Promise(function(resolve, reject) {
+		httpFetcher('npmsearch.com', `/query?q=${packageName}&fields=name,repository`, 'http')
+		  .then(res => {
+		    res.results.map(pkg => {
+		    	if(pkg.name[0] === packageName) {
+		    		found = true;
+		    		resolve(pkg);
+		    	}
+		    });
+				if(found === null) {
+					resolve(false);
+				}
+		  })
+		  .catch(err => {
+		  	reject(err);
+		  });
+	});
+};
