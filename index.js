ad6284dadb052dc9c4a76eb24cfc9b2bf0094d72
rajikaimal <inbox.rajika@gmail.com>
2016-09-11 08:10:58 +0530

    get total packages and request all

--- index.js
+++ index.js
@@ -5,17 +5,23 @@ module.exports = function(packageName) {
 	let found = null;
 
 	return new Promise(function(resolve, reject) {
-		httpFetcher('npmsearch.com', `/query?q=${packageName}&fields=name,repository`, 'http')
-		  .then(res => {
-		    res.results.map(pkg => {
-		    	if(pkg.name[0] === packageName) {
-		    		found = true;
-		    		resolve(pkg);
-		    	}
-		    });
-				if(found === null) {
-					resolve(false);
-				}
+		httpFetcher('npmsearch.com', `/query?q=${packageName}&fields=name`, 'http')
+		  .then(result => {
+				httpFetcher('npmsearch.com', `/query?q=${packageName}&fields=name,repository&size=${result.total}`, 'http')
+				  .then(res => {
+				    res.results.map(pkg => {
+				    	if(pkg.name[0] === packageName) {
+				    		found = true;
+				    		resolve(pkg);
+				    	}
+				    });
+						if(found === null) {
+							resolve(false);
+						}
+				  })
+				  .catch(err => {
+				  	reject(err);
+				  });
 		  })
 		  .catch(err => {
 		  	reject(err);
