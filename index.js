60a315093c79972617b840652b4fc6efaa4c609f
rajikaimal <inbox.rajika@gmail.com>
2017-04-20 23:49:14 +0530

    Use prettier

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-const httpFetcher = require('http-fetcher');
+const httpFetcher = require("http-fetcher");
 
 module.exports = function(packageName) {
-
 	let found = null;
 
 	return new Promise(function(resolve, reject) {
-		httpFetcher('npmsearch.com', `/query?q=${packageName}&fields=name`, 'http')
-		  .then(result => {
-				httpFetcher('npmsearch.com', `/query?q=${packageName}&fields=name,repository&size=${result.total}`, 'http')
-				  .then(res => {
-				    res.results.map(pkg => {
-				    	if(pkg.name[0] === packageName) {
-				    		found = true;
-				    		resolve(pkg);
-				    	}
-				    });
-						if(found === null) {
+		httpFetcher(
+			"npmsearch.com",
+			`/query?q=${packageName}&fields=name`,
+			"http"
+		)
+			.then(result => {
+				httpFetcher(
+					"npmsearch.com",
+					`/query?q=${packageName}&fields=name,repository&size=${result.total}`,
+					"http"
+				)
+					.then(res => {
+						res.results.map(pkg => {
+							if (pkg.name[0] === packageName) {
+								found = true;
+								resolve(pkg);
+							}
+						});
+						if (found === null) {
 							resolve(false);
 						}
-				  })
-				  .catch(err => {
-				  	reject(err);
-				  });
-		  })
-		  .catch(err => {
-		  	reject(err);
-		  });
+					})
+					.catch(err => {
+						reject(err);
+					});
+			})
+			.catch(err => {
+				reject(err);
+			});
 	});
 };
--- test/index.js
+++ test/index.js
@@ -1,19 +1,21 @@
-const assert = require('chai').assert;
-const npmChecker = require('../index.js');
+const assert = require("chai").assert;
+const npmChecker = require("../index.js");
 
-describe('test npm-checker', function() {
-	it('should return a resolved promise - package exists', function(done) {
-		npmChecker('http-fetcher')
+describe("test npm-checker", function() {
+	it("should return a resolved promise - package exists", function(done) {
+		npmChecker("http-fetcher")
 			.then(res => {
-				assert.typeOf(res, 'object');
+				assert.typeOf(res, "object");
 				done();
 			})
 			.catch(err => {
 				console.log(err);
 			});
 	});
-	it('should return a resolved promise - package doesn\'t exist', function(done) {
-		npmChecker('non-existing-package')
+	it("should return a resolved promise - package doesn't exist", function(
+		done
+	) {
+		npmChecker("non-existing-package")
 			.then(res => {
 				assert.equal(res, false);
 				done();
@@ -22,14 +24,16 @@ describe('test npm-checker', function() {
 				console.log(err);
 			});
 	});
-	it('should return a rejected promise - something wrong happened', function(done) {
-		npmChecker('non-existing-package')
+	it("should return a rejected promise - something wrong happened", function(
+		done
+	) {
+		npmChecker("non-existing-package")
 			.then(res => {
 				assert.equal(res, false);
 				done();
 			})
 			.catch(err => {
-				assert.typeOf(res, 'object');
+				assert.typeOf(res, "object");
 				done();
 			});
 	});
